14ae97629159ad886a8072a3dcb9c1a75268a2b0
xiaoqqq <magic_xiao@126.com>
2018-02-03 20:35:37 +0800

    dev fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,7 @@ Page({
         projectAsOfDate: '2018年3月1日',
         demandName: 'ui.chooseImage',
         demandDesc: 'V2.0支持图片多选功能',
-        demandAsOfDate: '2018年2月10日'
+        demandAsOfDate: '2018年3月10日'
     },
     onLoad: function () {
         "master branch"
