31eb2982c1ce2b92fbf3fae63a981adee54e624f
xiaoqqq <magic_xiao@126.com>
2018-02-03 20:53:46 +0800

    1. 新增代码块；2、修改日期

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,9 +9,13 @@ Page({
         projectAsOfDate: '2018年3月1日',
         demandName: 'ui.chooseImage',
         demandDesc: 'V2.0支持图片多选功能',
-        demandAsOfDate: '2018年3月10日'
+        demandAsOfDate: '2018年11月10日'
     },
     onLoad: function () {
-        
+        "master commit"
+
     },
+    test: function () {
+        // 这个是dev branch 上没有的代码块
+    }
 })
