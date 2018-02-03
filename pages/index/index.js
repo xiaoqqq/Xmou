7aefb889539580f51b898b48423072ca1727b3fe
xiaoqqq <magic_xiao@126.com>
2018-02-03 16:58:24 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    projectName: 'EPP-Android',
+    projectDesc: '企业门户平台Android客户端',
+    projectAsOfDate: '2018年3月1日',
+    demandName: 'ui.chooseImage',
+    demandDesc: 'V2.0支持图片多选功能',
+    demandAsOfDate: '2018年1月31日'
+  },
+  onLoad: function () {
+
+  },
+})
---
+++ pages/mine/mine.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+
+  },
+  onLoad: function () {
+    wx.setNavigationBarTitle({
+      title: '我的',
+    })
+  }
+})
